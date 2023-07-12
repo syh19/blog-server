e7d5dc1c1fa1327a9cc7120eac97e642940cde0b
xhir <syh1508@163.com>
2023-07-12 21:10:32 +0800

    1

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -12,7 +12,7 @@ const router = new Router({ prefix: '/user' })
 router.get('/test', (ctx, next) => {
   ctx.body = {
     code: 0,
-    data: '后端接口部署成功a3243453512l：' + new Date().getTime(),
+    data: '后端接口12l：' + new Date().getTime(),
     msg: '接口部署成功'
   }
 })
